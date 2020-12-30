be7c91ff85cc22682d7aedfdf60acbf51b393760
Erik Higginbotham <higgi615@ufl.edu>
2020-12-29 22:28:10 -0500

    WIP - regex for day4 part 2

--- src/day4.rs
+++ src/day4.rs
@@ -1,3 +1,10 @@
+use regex::Regex;
+use std::error::Error;
+
+const NUMBER_MATCHER: Regex = Regex::new(r"/\d+/").unwrap();
+const CM_MATCHER: Regex = Regex::new(r"/\d+cm/gm").unwrap();
+const IN_MATCHER: Regex = Regex::new(r"/\d+cm/gm").unwrap();
+
 fn get_input() -> Vec<&'static str> {
     include_str!("./inputs/day4.txt").split("\n\n").collect()
 }
@@ -7,7 +14,7 @@ fn parse_input(input: Vec<&str>) -> Vec<Vec<&str>> {
 }
 
 /// go through entries and determine if the passport is valid
-fn validate_passport(passport: &Vec<&str>) -> bool {
+fn validate_passport_pt_1(passport: &Vec<&str>) -> bool {
     let mut field_counter = 0;
     for key in passport.iter() {
         match &key[..3] {
@@ -25,11 +32,53 @@ fn validate_passport(passport: &Vec<&str>) -> bool {
     field_counter == 7
 }
 
+/// go through entries and determine if the passport is valid
+fn validate_passport_pt_2(passport: &Vec<&str>) -> bool {
+    let mut field_counter = 0;
+    for key in passport.iter() {
+        let value = &key[3..];
+        match &key[..3] {
+            "byr" => {
+                if value.len() == 4
+                    && (value.parse().unwrap() >= 1920
+                    && value.parse().unwrap() <= 2002) {
+                    field_counter += 1;
+                }
+            },
+            "iyr" => {
+                if value.len() == 4
+                    && (value.parse().unwrap() >= 2010
+                    && value.parse().unwrap() <= 2020) {
+                    field_counter += 1;
+                }
+            },
+            "eyr" => {
+                if value.len() == 4
+                    && (value.parse().unwrap() >= 2020
+                    && value.parse().unwrap() <= 2030) {
+                    field_counter += 1;
+                }
+            },
+            "hgt" => {
+                if cm_matcher.is_match(value) {
+
+                }
+            },
+            "hcl" => field_counter += 1,
+            "ecl" => field_counter += 1,
+            "pid" => field_counter += 1,
+            _ => ()
+        }
+    }
+
+    field_counter == 7
+}
+
 pub fn get_solution_pt_1() -> i32 {
     let parsed_input = parse_input(get_input());
     let mut valid_passports = 0;
     for passport in parsed_input.iter() {
-        if validate_passport(passport) {
+        if validate_passport_pt_1(passport) {
             valid_passports += 1;
         }
     }
