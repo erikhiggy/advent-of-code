b27cf8901ec1a05995e5cff55c6a21aac472f7a5
Erik Higginbotham <higgi615@ufl.edu>
2020-12-29 21:14:21 -0500

    Day 4 Part 1 complete

--- src/day4.rs
+++ src/day4.rs
@@ -1,11 +1,38 @@
-pub fn get_input() -> Vec<&'static str> {
-    include_str!("./inputs/day4.txt").lines().collect()
+fn get_input() -> Vec<&'static str> {
+    include_str!("./inputs/day4.txt").split("\n\n").collect()
 }
 
-pub fn parse_input(input: Vec<&str>) -> Vec<Vec<&str>> {
-    let mut parsed_input: Vec<Vec<&str>> = input
-        .iter()
-        .map(|item| item.split_whitespace().collect()).collect();
+fn parse_input(input: Vec<&str>) -> Vec<Vec<&str>> {
+    input.iter().map(|line| line.split_whitespace().collect()).collect()
+}
 
-    parsed_input.iter().filter_map(|s: &Vec<&str>| Some(!s.is_empty())).collect()
+/// go through entries and determine if the passport is valid
+fn validate_passport(passport: &Vec<&str>) -> bool {
+    let mut field_counter = 0;
+    for key in passport.iter() {
+        match &key[..3] {
+            "byr" => field_counter += 1,
+            "iyr" => field_counter += 1,
+            "eyr" => field_counter += 1,
+            "hgt" => field_counter += 1,
+            "hcl" => field_counter += 1,
+            "ecl" => field_counter += 1,
+            "pid" => field_counter += 1,
+            _ => ()
+        }
+    }
+
+    field_counter == 7
+}
+
+pub fn get_solution_pt_1() -> i32 {
+    let parsed_input = parse_input(get_input());
+    let mut valid_passports = 0;
+    for passport in parsed_input.iter() {
+        if validate_passport(passport) {
+            valid_passports += 1;
+        }
+    }
+
+    valid_passports
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
-use crate::day4::get_input;
-
 mod day1;
 mod day2;
 mod day3;
 mod day4;
 
 fn main() {
-    // let day1_solution_pt_1 = day1::solution_pt_1();
-    // let day1_solution_pt_2 = day1::solution_pt_2();
-    //
-    // println!("Day 1 Solution Part 1: {}", day1_solution_pt_1);
-    // println!("Day 1 Solution Part 2: {}", day1_solution_pt_2);
-    //
-    // let day2_solution_pt_1 = day2::get_solution_part1();
-    // let day2_solution_pt_2 = day2::get_solution_part2();
-    //
-    // println!("Day 2 Solution Part 1: {}", day2_solution_pt_1);
-    // println!("Day 2 Solution Part 2: {}", day2_solution_pt_2);
-    //
-    // let day3_solution_pt_1 = day3::get_solution_part1();
-    // let day3_solution_pt_2 = day3::get_solution_part2();
-    //
-    // println!("Day 3 Solution Part 1: {}", day3_solution_pt_1);
-    // println!("Day 3 Solution Part 2: {}", day3_solution_pt_2);
-    println!("{:?}", day4::parse_input(get_input()));
+    let day1_solution_pt_1 = day1::solution_pt_1();
+    let day1_solution_pt_2 = day1::solution_pt_2();
+
+    println!("Day 1 Solution Part 1: {}", day1_solution_pt_1);
+    println!("Day 1 Solution Part 2: {}", day1_solution_pt_2);
+
+    let day2_solution_pt_1 = day2::get_solution_part1();
+    let day2_solution_pt_2 = day2::get_solution_part2();
+
+    println!("Day 2 Solution Part 1: {}", day2_solution_pt_1);
+    println!("Day 2 Solution Part 2: {}", day2_solution_pt_2);
+
+    let day3_solution_pt_1 = day3::get_solution_part1();
+    let day3_solution_pt_2 = day3::get_solution_part2();
+
+    println!("Day 3 Solution Part 1: {}", day3_solution_pt_1);
+    println!("Day 3 Solution Part 2: {}", day3_solution_pt_2);
+
+    let day4_solution_pt_1 = day4::get_solution_pt_1();
+
+    println!("Day 4 Solution Part 1: {}", day4_solution_pt_1);
 }
