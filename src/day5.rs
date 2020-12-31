404872a0d6c38ed199e241a2f7cec5c3f400147d
Erik Higginbotham <erik96@me.com>
2020-12-31 18:49:39 -0500

    day 5 pt 2 in progress

--- src/day5.rs
+++ src/day5.rs
@@ -1,3 +1,5 @@
+use core::num::flt2dec::decode;
+
 fn get_input() -> Vec<&'static str> {
     include_str!("./inputs/day5.txt").lines().collect()
 }
@@ -37,13 +39,32 @@ fn decode_seat_string(seat_string: &str) -> (Vec<i32>, Vec<i32>) {
     (rows, cols)
 }
 
+fn get_sid(seat_number: (Vec<i32>, Vec<i32>)) -> i32 {
+    seat_number.0[0] * 8 as i32 + seat_number.1[0]
+}
+
 pub fn get_solution_pt_1() -> i32 {
     let mut max = 0;
-    for plane in get_input().iter() {
-        let answer_tuple = decode_seat_string(plane);
-        if answer_tuple.0[0] * 8 as i32 + answer_tuple.1[0] > max {
-            max = answer_tuple.0[0] * 8 as i32 + answer_tuple.1[0];
+    for line in get_input().iter() {
+        let sid = get_sid(decode_seat_string(line));
+        if sid > max {
+            max = sid;
         }
     }
     max
+}
+
+pub fn get_solution_part_2() -> i32 {
+    let mut sid_vec: Vec<i32> = Vec::new();
+    for line in get_input().iter() {
+        let sid = get_sid(decode_seat_string(line));
+        sid_vec.push(sid);
+    }
+
+    sid_vec.sort();
+
+    for sid in sid_vec.iter() {
+
+    }
+    10
 }
