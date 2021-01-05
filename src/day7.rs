06c7f3acb3d1691f22941b45e000fd803f26317d
Erik Higginbotham <erik96@me.com>
2021-01-05 09:44:00 -0500

    WIP - Day 7 Part 2

--- src/day7.rs
+++ src/day7.rs
@@ -37,7 +37,39 @@ fn find_shiny_gold(input: Vec<&str>) -> usize {
     shiny_gold_count
 }
 
+fn shiny_gold_pt_2(input: Vec<&str>) -> usize {
+    let mut shiny_gold_bags: usize = 0;
+    let mut bags_vec: Vec<String> = Vec::new();
+    // push the initial bags into the list
+    bags_vec.push(String::from("shiny gold"));
+
+    // while there are till bags to go through
+    while !bags_vec.is_empty() {
+        // save a copy of the bags vec for iteration purposes
+        let temp_vec = bags_vec.clone();
+        println!("Bags Vec: {:?}", bags_vec);
+        // clear the bags vec to prepare for the new bags
+        bags_vec.clear();
+        for bag in temp_vec.iter() {
+            for line in input.iter() {
+                if line.starts_with(bag) {
+                    let new_bags: String = line[line.chars().find(|x| x.is_digit(10))..line.find("bag").unwrap() - 1].parse().unwrap();
+                    let mut new_bags_vec: Vec<String> = new_bags.chars().split(',');
+                    bags_vec = new_bags_vec;
+                }
+            }
+        }
+    }
+
+    shiny_gold_bags
+}
+
 pub fn get_solution_pt_1() -> usize {
     let input = get_input();
     find_shiny_gold(input)
+}
+
+pub fn get_solution_pt_2() -> usize {
+    let input = get_input();
+    shiny_gold_pt_2(input)
 }
--- src/main.rs
+++ src/main.rs
@@ -7,43 +7,44 @@ mod day6;
 mod day7;
 
 fn main() {
-    let day1_solution_pt_1 = day1::solution_pt_1();
-    let day1_solution_pt_2 = day1::solution_pt_2();
-
-    println!("Day 1 Solution Part 1: {}", day1_solution_pt_1);
-    println!("Day 1 Solution Part 2: {}", day1_solution_pt_2);
-
-    let day2_solution_pt_1 = day2::get_solution_part1();
-    let day2_solution_pt_2 = day2::get_solution_part2();
-
-    println!("Day 2 Solution Part 1: {}", day2_solution_pt_1);
-    println!("Day 2 Solution Part 2: {}", day2_solution_pt_2);
-
-    let day3_solution_pt_1 = day3::get_solution_part1();
-    let day3_solution_pt_2 = day3::get_solution_part2();
-
-    println!("Day 3 Solution Part 1: {}", day3_solution_pt_1);
-    println!("Day 3 Solution Part 2: {}", day3_solution_pt_2);
-
-    let day4_solution_pt_1 = day4::get_solution_pt_1();
-    let day4_solution_pt_2 = day4::get_solution_pt_2();
-
-    println!("Day 4 Solution Part 1: {}", day4_solution_pt_1);
-    println!("Day 4 Solution Part 2: {:?}", day4_solution_pt_2);
-
-    let day5_solution_pt_1 = day5::get_solution_pt_1();
-    let day5_solution_pt_2 = day5::get_solution_pt_2();
-
-    println!("Day 5 Solution Part 1: {:?}", day5_solution_pt_1);
-    println!("Day 5 Solution Part 2: {:?}", day5_solution_pt_2);
-
-    let day6_solution_pt_1 = day6::get_solution_pt_1();
-    let day6_solution_pt_2 = day6::get_solution_pt_2();
-
-    println!("Day 6 Solution Part 1: {:?}", day6_solution_pt_1);
-    println!("Day 6 Solution Part 2: {:?}", day6_solution_pt_2);
-
-    let day7_solution_pt_1 = day7::get_solution_pt_1();
-
-    println!("Day 7 Solution Part 1: {:?}", day7_solution_pt_1);
+    // let day1_solution_pt_1 = day1::solution_pt_1();
+    // let day1_solution_pt_2 = day1::solution_pt_2();
+    //
+    // println!("Day 1 Solution Part 1: {}", day1_solution_pt_1);
+    // println!("Day 1 Solution Part 2: {}", day1_solution_pt_2);
+    //
+    // let day2_solution_pt_1 = day2::get_solution_part1();
+    // let day2_solution_pt_2 = day2::get_solution_part2();
+    //
+    // println!("Day 2 Solution Part 1: {}", day2_solution_pt_1);
+    // println!("Day 2 Solution Part 2: {}", day2_solution_pt_2);
+    //
+    // let day3_solution_pt_1 = day3::get_solution_part1();
+    // let day3_solution_pt_2 = day3::get_solution_part2();
+    //
+    // println!("Day 3 Solution Part 1: {}", day3_solution_pt_1);
+    // println!("Day 3 Solution Part 2: {}", day3_solution_pt_2);
+    //
+    // let day4_solution_pt_1 = day4::get_solution_pt_1();
+    // let day4_solution_pt_2 = day4::get_solution_pt_2();
+    //
+    // println!("Day 4 Solution Part 1: {}", day4_solution_pt_1);
+    // println!("Day 4 Solution Part 2: {:?}", day4_solution_pt_2);
+    //
+    // let day5_solution_pt_1 = day5::get_solution_pt_1();
+    // let day5_solution_pt_2 = day5::get_solution_pt_2();
+    //
+    // println!("Day 5 Solution Part 1: {:?}", day5_solution_pt_1);
+    // println!("Day 5 Solution Part 2: {:?}", day5_solution_pt_2);
+    //
+    // let day6_solution_pt_1 = day6::get_solution_pt_1();
+    // let day6_solution_pt_2 = day6::get_solution_pt_2();
+    //
+    // println!("Day 6 Solution Part 1: {:?}", day6_solution_pt_1);
+    // println!("Day 6 Solution Part 2: {:?}", day6_solution_pt_2);
+    //
+    // let day7_solution_pt_1 = day7::get_solution_pt_1();
+       let day7_solution_pt_2 = day7::get_solution_pt_2();
+    //
+    // println!("Day 7 Solution Part 1: {:?}", day7_solution_pt_1);
 }
