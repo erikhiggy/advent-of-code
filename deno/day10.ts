2821c4150bcdeaaaa34e1fd1b83dc6026a356c50
Erik Higginbotham <higgi615@ufl.edu>
2021-03-23 22:41:25 -0400

    day 10 done

--- deno/day10.ts
+++ deno/day10.ts
@@ -3,13 +3,14 @@ function parseInput(input: string) {
 }
 
 let joltageRatings: number[] = parseInput(await Deno.readTextFile('./inputs/day10.txt'));
+joltageRatings.unshift(0);
+joltageRatings.sort((a: number, b: number) => a - b);
 
 function part1(joltageRatings: number[]) {
   let outlet = 0;
   let diff1 = 0;
   let diff3 = 1; // bc of the highest rated joltage rule
 
-  joltageRatings.sort((a: number, b: number) => a - b);
   joltageRatings.forEach((rating: number) => {
     if (outlet + 1 === rating) {
       diff1 += 1;
@@ -19,7 +20,27 @@ function part1(joltageRatings: number[]) {
 
     outlet = rating;
   });
-  return diff1;
+  return diff1 * diff3;
+}
+
+let valueMap = new Map();
+
+function part2(num: number) {
+  if (num === (joltageRatings.length - 1)) return 1;
+  if (valueMap.has(num)) {
+    // console.log(valueMap.get(num));
+    return valueMap.get(num);
+  }
+
+  let ans = 0;
+  let j = num + 1;
+  for (; j < joltageRatings.length; j++) {
+    if (joltageRatings[j] - joltageRatings[num] > 3) break;
+    ans += part2(j);
+  }
+  valueMap.set(num, ans);
+  return ans;
 }
 
 console.log(part1(joltageRatings));
+console.log(part2(0));
